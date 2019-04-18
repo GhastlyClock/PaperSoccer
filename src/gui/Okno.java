8d181c93097626e8a66b50c183cf803025f829ff
Gašper Mrzelj <gasper.mrzelj@gmail.com>
2019-04-18 10:09:17 +0200

    tocka

--- src/gui/Okno.java
+++ src/gui/Okno.java
@@ -2,6 +2,7 @@ package gui;
 
 import javax.swing.JFrame;
 
+@SuppressWarnings("serial")
 public class Okno extends JFrame {
 	
 	Platno platno;
--- src/logika/Tocka.java
+++ src/logika/Tocka.java
@@ -4,36 +4,43 @@ import java.util.*;
 
 public class Tocka {
 	
-	public int x,y;
+	// Dogovor: y predstavlja vrstico, x predstavlja stolpec
+	int x, y;
+	public static final Set<int[]> mozniPremiki;
+	public Set<int[]> veljavnePoteze;
 	
-	public Set<Vector<Integer>> mozniPremiki;
+	static {
+		
+		// Ustvarim množico vseh možnih premikov, katero bomo uporabljali pri vsaki potezi
+		mozniPremiki = new HashSet<>();
+		for (int i = -1; i <= 1; ++i) {
+			for (int j = -1; j <= 1; ++j) {
+				int[] v = {i, j};
+				mozniPremiki.add(v);
+			}
+		}
+		int[] n = {0,0};
+		mozniPremiki.remove(n);
+	}
 	
-	public Tocka(int x,int y) {
+	public Tocka(int x, int y) {
+		// Vsaki tocki bomo morali podati položaj, ki bo enolicno dolocal tocko
 		this.x = x;
 		this.y = y;
+	}
+	
+	public void veljavnePoteze (int stVrstic, int stStolpcev) {
 		
-		mozniPremiki = new HashSet<Vector<Integer>>();
-		if (x != 0 && x != 10 && x != 1 && x != 9 && y != 0 && y != 6) {
-			Vector<Integer> v = new Vector<Integer>();
-			v.add(0, 1);
-			v.add(1, 1);
-			mozniPremiki.add(v);
-			v.add(1,0);
-			mozniPremiki.add(v);
-			v.add(1,-1);
-			mozniPremiki.add(v);
-			v.add(0,0);
-			mozniPremiki.add(v);
-			v.add(1,1);
-			mozniPremiki.add(v);
-			v.add(0,-1);
-			mozniPremiki.add(v);
-			v.add(1,0);
-			mozniPremiki.add(v);
-			v.add(1,-1);
-			mozniPremiki.add(v);
+		// Na zacetku v množico veljavnih potez dodamo vse možne premike
+		for (int[] premik : mozniPremiki) veljavnePoteze.add(premik);
+		
+		// Moramo pogledati v katero smer je pomik veljaven, ce ni ga odstranimo
+		// 1. Pogledamo ali tocka leži na katerem od levem ali desnem robu
+		if (x == 0) {
+			for (int i = -1; i < 1; ++i) {
+				// Do something....
+			}
 		}
-		else ;
 	}
 	
 	

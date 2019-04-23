29d345b38c6dfe0a0c6ca506404f5dfe10872451
Gašper Mrzelj <gasper.mrzelj@gmail.com>
2019-04-23 12:34:03 +0200

    .

--- src/PaperSoccer.java
+++ src/PaperSoccer.java
@@ -1,21 +1,8 @@
 import gui2.*;
-import logika.*;
 
 public class PaperSoccer {
 
-	public static void main(String[] args) {
-		
-		Igra igra = new Igra();
-		for (Polje[] vrstica : igra.getPlosca()) {
-			for (Polje p : vrstica) {
-				System.out.print(p + " ima "+ p.veljavnePoteze.size() + " povezave: ");
-				for (Premik premik : p.veljavnePoteze) {
-					System.out.print(premik + " ");
-				}
-				System.out.println();
-			}
-		}
-		
+	public static void main(String[] args) {	
 		
 		Okno okno = new Okno();
 		okno.pack();
